8bd76b12334089f353f706ad9524a8749356ce36
Aliaksandr Hruntou <punkypong1@gmail.com>
2024-01-25 23:38:54 +0300

    feat: add cli env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    let args = [];
+    for (let n in process.env) {
+        if (n.startsWith("RSS_")) args.push(`${n}=${process.env[n]}`)
+    }
+    console.log(args.join("; "));
 };
 
 parseEnv();
