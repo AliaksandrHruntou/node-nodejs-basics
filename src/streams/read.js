996cc561a056b4146513dda2eafa85230260a450
Aliaksandr Hruntou <punkypong1@gmail.com>
2024-01-27 01:06:21 +0300

    feat: add streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import * as path from 'node:path';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const content = createReadStream(filePath);
+
+    content.pipe(stdout);
 };
 
 await read();
