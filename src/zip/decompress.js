c7871469c54166e9e4f4a078a83665eb71a3f126
Aliaksandr Hruntou <punkypong1@gmail.com>
2024-01-29 00:22:32 +0300

    feat: add zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'archive.gz');
+const unzipFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        await pipeline(
+            createReadStream(filePath),
+            createGunzip(),
+            createWriteStream(unzipFilePath),
+        );
+        console.log('Decompress succeeded.');
+    } catch (error) {
+        console.log(error);
+    } 
 };
 
 await decompress();
