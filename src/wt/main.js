b18513b4e9313b31eda513a18ac5c3abedfb6a11
Aliaksandr Hruntou <punkypong1@gmail.com>
2024-01-29 03:07:53 +0300

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const workerPath = path.join(__dirname, 'worker.js');
+
+let initNum = 10;
+
+function runService(workerData) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, { workerData });
+      worker.on('message', data => resolve({
+        status: "resolved",
+        data
+      }));
+      worker.on('error', () => resolve({
+        status: "error",
+        data: null
+      }));
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      })
+    })
+  }
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+    for (let i = 0; i < cpus().length; i++) {
+        workers.push(runService(initNum + i));
+    }
+    const resultsArray = await Promise.all(workers);
+    console.log(resultsArray);
 };
 
 await performCalculations();
